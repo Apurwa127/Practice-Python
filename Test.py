db5e052cb644673171b6c9f848cf8866399c1ea9
Apurwa Bhattarai <bhattarai.apurba@gmail.com>
2025-03-06 18:59:25 -0600

    first commit

--- Test.py
+++ Test.py
@@ -1,9 +1,13 @@
 import re
 
 chat1 = 'codebasics: you ask lot of questions 12345678912, abc@xyz.com'
-chat2 = 'codebasics: here it is: (123)-567-8912,1234567891, abx@xyz.com'
+chat2 = 'codebasics: here it is: (123)-567-8912,1234567891,123-456-7891 abx@xyz.com'
 chat3 = 'codebasics: yes, phone: 12345678912 email: abx@xyz.com'
 
-pattern = '\d{10}| \(\d{3}\)-\d{3}-\d{4}'
-matches = re.findall(pattern, chat2)
-print(matches)
+
+pattern = r'\(\d{3}\)-\d{3}-\d{4}|\d{10}|\d{3}-\d{3}-\d{4}'
+pattern2 = r'[a-z0-9A-Z_]*@[a-z]*\.[a-z]*'
+output = re.findall(pattern, chat2)
+output2 = re.findall(pattern2, chat3)
+print(output)
+print(output2)
